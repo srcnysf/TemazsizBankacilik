d18ad2118234b182097c854a0e1be41e2e8d3c85
Okan AYDIN <okanaydin1994@gmail.com>
2016-11-06 01:32:29 +0200

    Alisveris Sonucu

--- mobile/src/main/java/com/example/sercanyusuf/temazsizbankacilik/AlisverisSonucu.java
+++ mobile/src/main/java/com/example/sercanyusuf/temazsizbankacilik/AlisverisSonucu.java
@@ -1,15 +1,23 @@
 package com.example.sercanyusuf.temazsizbankacilik;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AlisverisSonucu extends AppCompatActivity {
 
     private Button btnCard1, btnCard2, btnCard3, btnOdemeYap;
-    private TextView tvFiyat, tvIban, tvFirmaAdi;
+    private TextView tvFiyat, tvFirmaAdi;
+
+    int tutar;
+
+    String[] hesaplar=new String[3];
+    String secilenHesap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +30,21 @@ public class AlisverisSonucu extends AppCompatActivity {
         btnOdemeYap=(Button)findViewById(R.id.btnOdemeYap);
 
         tvFiyat=(TextView)findViewById(R.id.tvFiyat);
-        tvIban=(TextView)findViewById(R.id.tvIban);
         tvFirmaAdi=(TextView)findViewById(R.id.tvFirmaAdi);
+
+
+        Bundle paket= getIntent().getExtras();
+        String firmaAdi= paket.getString("firmaAdi");
+        tutar = Integer.parseInt(paket.getString("tutar"));
 
 
         btnCard1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                secilenHesap="1000";
+
+                Toast.makeText(getApplicationContext(),"Kart 1 seçildi",Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -55,24 +70,49 @@ public class AlisverisSonucu extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                alert("BİLGİLENDİRME", "Ödemenizi Onaylıyor Musunuz?");
 
             }
         });
 
-        Bundle paket= getIntent().getExtras();
-        String iban= paket.getString("iban");
-        String firmaAdi= paket.getString("firmaAdi");
-       int tutar = Integer.parseInt(paket.getString("tutar"));
+
+
 
 
         tvFiyat.setText(String.valueOf(tutar));
-        tvIban.setText(iban);
         tvFirmaAdi.setText(firmaAdi);
 
 
+    }
 
 
+
+    public void alert(String title, String Message){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+
+        builder.setPositiveButton("EVET", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Finans f=new Finans();
+                f.bakiyeDusur(tutar);
+                Toast.makeText(getApplicationContext(),"Ödemeniz Başarı ile Gerçekleşti :)", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("İPTAL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"İptal Edildi", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.show();
+
+    }
 
 
     }
-}
+
