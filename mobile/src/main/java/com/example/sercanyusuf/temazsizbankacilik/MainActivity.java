17527359f292ae4c29b2e1d18466fb673487cc19
sercanyusuf <sercan0708@gmail.com>
2016-11-06 02:18:17 +0300

    as

--- mobile/src/main/java/com/example/sercanyusuf/temazsizbankacilik/MainActivity.java
+++ mobile/src/main/java/com/example/sercanyusuf/temazsizbankacilik/MainActivity.java
@@ -1,16 +1,17 @@
 package com.example.sercanyusuf.temazsizbankacilik;
 
-import android.graphics.PointF;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
-import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+import com.cooltechworks.creditcarddesign.CardEditActivity;
+import com.cooltechworks.creditcarddesign.CreditCardUtils;
+import com.cooltechworks.creditcarddesign.CreditCardView;
 
-public class MainActivity extends AppCompatActivity implements QRCodeReaderView.OnQRCodeReadListener{
+public class MainActivity extends AppCompatActivity {
 
-    private TextView resultTextView;
-    private QRCodeReaderView qrCodeReaderView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,44 +19,83 @@ public class MainActivity extends AppCompatActivity implements QRCodeReaderView.
         setContentView(R.layout.activity_main);
 
 
-        qrCodeReaderView = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
-        resultTextView=(TextView)findViewById(R.id.textView);
+        findViewById(R.id.add_card).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CardEditActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    private void initListener(final int index, CreditCardView creditCardView) {
 
 
-        qrCodeReaderView.setOnQRCodeReadListener(this);
+        creditCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        // Use this function to enable/disable decoding
-        qrCodeReaderView.setQRDecodingEnabled(true);
+                CreditCardView creditCardView = (CreditCardView) v;
+                String cardNumber = creditCardView.getCardNumber();
+                String expiry = creditCardView.getExpiry();
+                String cardHolderName = creditCardView.getCardHolderName();
 
-        // Use this function to change the autofocus interval (default is 5 secs)
-        qrCodeReaderView.setAutofocusInterval(2000L);
+                Intent intent = new Intent(MainActivity.this, CardEditActivity.class);
+                intent.putExtra(CreditCardUtils.EXTRA_CARD_HOLDER_NAME, cardHolderName);
+                intent.putExtra(CreditCardUtils.EXTRA_CARD_NUMBER, cardNumber);
+                intent.putExtra(CreditCardUtils.EXTRA_CARD_EXPIRY, expiry);
+                intent.putExtra(CreditCardUtils.EXTRA_CARD_SHOW_CARD_SIDE, CreditCardUtils.CARD_SIDE_FRONT);
+                intent.putExtra(CreditCardUtils.EXTRA_VALIDATE_EXPIRY_DATE, false);
 
-        // Use this function to enable/disable Torch
-        qrCodeReaderView.setTorchEnabled(true);
 
-        // Use this function to set front camera preview
-        qrCodeReaderView.setFrontCamera();
+                startActivityForResult(intent, index);
 
-        // Use this function to set back camera preview
-        qrCodeReaderView.setBackCamera();
-
+            }
+        });
 
     }
 
-    @Override
-    public void onQRCodeRead(String text, PointF[] points) {
-        resultTextView.setText(text);
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        qrCodeReaderView.startCamera();
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        qrCodeReaderView.stopCamera();
-    }
-}
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+
+        if (resultCode == RESULT_OK) {
+
+            LinearLayout cardContainer = (LinearLayout) findViewById(R.id.card_container);
+
+
+            String name = data.getStringExtra(CreditCardUtils.EXTRA_CARD_HOLDER_NAME);
+            String cardNumber = data.getStringExtra(CreditCardUtils.EXTRA_CARD_NUMBER);
+            String expiry = data.getStringExtra(CreditCardUtils.EXTRA_CARD_EXPIRY);
+            String cvv = data.getStringExtra(CreditCardUtils.EXTRA_CARD_CVV);
+
+
+            if (reqCode == 0) {
+
+
+                CreditCardView creditCardView = new CreditCardView(this);
+
+                creditCardView.setCVV(cvv);
+                creditCardView.setCardHolderName(name);
+                creditCardView.setCardExpiry(expiry);
+                creditCardView.setCardNumber(cardNumber);
+
+                int index = cardContainer.getChildCount();
+                cardContainer.addView(creditCardView);
+                initListener(index, creditCardView);
+
+
+            } else {
+
+                CreditCardView creditCardView = (CreditCardView) cardContainer.getChildAt(reqCode);
+
+                creditCardView.setCardExpiry(expiry);
+                creditCardView.setCardNumber(cardNumber);
+                creditCardView.setCardHolderName(name);
+                creditCardView.setCVV(cvv);
+
+            }
+        }
+
+
+    }}
