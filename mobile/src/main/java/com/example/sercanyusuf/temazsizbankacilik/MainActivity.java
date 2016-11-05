0dda7d4f809253637af351c3c058d6a3915183b0
Okan AYDIN <okanaydin1994@gmail.com>
2016-11-05 16:52:19 +0200

    QRCodeReaderView

--- mobile/src/main/java/com/example/sercanyusuf/temazsizbankacilik/MainActivity.java
+++ mobile/src/main/java/com/example/sercanyusuf/temazsizbankacilik/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.sercanyusuf.temazsizbankacilik;
 
+import android.graphics.PointF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+
+public class MainActivity extends AppCompatActivity implements QRCodeReaderView.OnQRCodeReadListener{
+
+    private TextView resultTextView;
+    private QRCodeReaderView qrCodeReaderView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,7 +18,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        qrCodeReaderView = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
+        resultTextView=(TextView)findViewById(R.id.textView);
 
 
+        qrCodeReaderView.setOnQRCodeReadListener(this);
+
+        // Use this function to enable/disable decoding
+        qrCodeReaderView.setQRDecodingEnabled(true);
+
+        // Use this function to change the autofocus interval (default is 5 secs)
+        qrCodeReaderView.setAutofocusInterval(2000L);
+
+        // Use this function to enable/disable Torch
+        qrCodeReaderView.setTorchEnabled(true);
+
+        // Use this function to set front camera preview
+        qrCodeReaderView.setFrontCamera();
+
+        // Use this function to set back camera preview
+        qrCodeReaderView.setBackCamera();
+
+
+    }
+
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        resultTextView.setText(text);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        qrCodeReaderView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        qrCodeReaderView.stopCamera();
     }
 }
